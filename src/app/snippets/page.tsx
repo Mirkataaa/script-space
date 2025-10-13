b7dbe454304fc4de45557aace67b0914f159a6fa
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-10-13 09:55:06 +0300

    feat: Add query to fetch all snippets & snippets page

---
+++ src/app/snippets/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
+import NavigationHeaderComponent from "@/components/NavigationHeaderComponent";
+
+export default function SnippetsPage() {
+  const snippets = useQuery(api.snippets.getSnippets);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null);
+  const [view, setView] = useState<"grid" | "list">("grid");
+
+  if (snippets === undefined) {
+    return (
+      <div className="min-h-screen">
+        <NavigationHeaderComponent />
+        {/* add skeleton here */}
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen bg-[#0a0a0f]">
+      <NavigationHeaderComponent />
+
+      <div className="relative max-w-7xl mx-auto px-4 py-12">
+        {/* hero seciton */}
+      </div>
+    </div>
+  );
+}
