ce28f78a2cef576401ae3be02deee9ca8d76a347
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-10-13 17:01:23 +0300

    feat(snippets): Add is snippetStarred and getSnippetsStarCount queries

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { q } from "motion/react-client";
 
 export const createSnippet = mutation({
   args: {
@@ -115,5 +116,45 @@ export const starSnippet = mutation({
         snippetId: args.snippetId,
       });
     }
+  },
+});
+
+export const isSnippetStarred = query({
+  args: {
+    snippetId: v.id("snippets"),
+  },
+
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) return false;
+
+    const star = await ctx.db
+      .query("stars")
+      .withIndex("by_user_id_and_snippet_id")
+      .filter(
+        (q) =>
+          q.eq(q.field("userId"), identity.subject) &&
+          q.eq(q.field("snippetId"), args.snippetId)
+      )
+      .first();
+
+    // return boolen
+    return !!star;
+  },
+});
+
+export const getSnippetsStarCount = query({
+  args: {
+    snippetId: v.id("snippets"),
+  },
+
+  handler: async (ctx, args) => {
+    const stars = await ctx.db
+      .query("stars")
+      .withIndex("by_snippet_id")
+      .filter((q) => q.eq(q.field("snippetId"), args.snippetId))
+      .collect();
+
+    return stars.length;
   },
 });
