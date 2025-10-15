2ad96d508d6164048ee4c833667f4d566e206717
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-10-15 22:13:05 +0300

    feat(user-profile): Add user information in profile header

--- src/app/profile/_components/ProfileHeaderComponent.tsx
+++ src/app/profile/_components/ProfileHeaderComponent.tsx
@@ -1,5 +1,7 @@
+import { UserIcon, Zap } from "lucide-react";
 import { Id } from "../../../../convex/_generated/dataModel";
 import { UserResource } from "@clerk/types";
+import Image from "next/image";
 
 interface ProfileHeaderProps {
   userStats: {
@@ -30,9 +32,51 @@ export default function ProfileHeaderComponent({
   userData,
   user,
 }: ProfileHeaderProps) {
-    console.log(userStats);
-    console.log(userData);
-    console.log(user);
-    
-  return <div>hello from profile header</div>;
+  return (
+    <div
+      className="relative mb-8 bg-gradient-to-br from-[#12121a] to-[#1a1a2e] rounded-2xl p-8 border
+     border-gray-800/50 overflow-hidden"
+    >
+      <div className="absolute inset-0 bg-grid-white/[0.02] bg-[size:32px]" />
+
+      {/* user info */}
+      <div className="relative flex items-center gap-8">
+        <div className="relative group">
+          <div
+            className="absolute inset-0 bg-gradient-to-r from-blue-500 to-purple-600 rounded-full 
+          blur-xl opacity-50 group-hover:opacity-75 transition-opacity"
+          />
+          <Image
+            src={user!.imageUrl}
+            alt="Profile"
+            className="w-24 h-24 rounded-full border-4 border-gray-800/50 relative z-10 group-hover:scale-105 transition-transform"
+            width={24}
+            height={24}
+          />
+          {userData.isPremium && (
+            <div
+              className="absolute -top-2 -right-2 bg-gradient-to-r from-purple-500 to-purple-600 p-2
+             rounded-full z-20 shadow-lg animate-pulse"
+            >
+              <Zap className="w-4 h-4 text-white" />
+            </div>
+          )}
+        </div>
+        <div>
+          <div className="flex items-center gap-3 mb-2">
+            <h1 className="text-3xl font-bold text-white">{userData.name}</h1>
+            {userData.isPremium && (
+              <span className="px-3 py-1 bg-purple-500/10 text-purple-400 rounded-full text-sm font-medium">
+                Premium Member
+              </span>
+            )}
+          </div>
+          <p className="text-gray-400 flex items-center gap-2">
+            <UserIcon className="w-4 h-4" />
+            {userData.email}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
