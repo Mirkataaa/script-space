7e14ff684fd7c6c08d6ee64071051957134df1bd
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-10-15 16:17:50 +0300

    feat(comments): Add delete comment mutation & handle delete comment in comments component

--- src/app/snippets/[id]/_components/CommentsCompponent.tsx
+++ src/app/snippets/[id]/_components/CommentsCompponent.tsx
@@ -6,6 +6,7 @@ import { api } from "../../../../../convex/_generated/api";
 import toast from "react-hot-toast";
 import { MessageSquare } from "lucide-react";
 import CommentFormComponent from "./CommentFormComponent";
+import CommentComponent from "./CommentComponent";
 
 export default function CommentsCompponent({
   snippetId,
@@ -14,8 +15,10 @@ export default function CommentsCompponent({
 }) {
   const { user } = useUser();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [deletingCommentId, setDeletingCommentId] = useState<string | null>(null);
   const comments = useQuery(api.snippets.getComments, { snippetId }) || [];
   const addComment = useMutation(api.snippets.addComment);
+  const deleteComment = useMutation(api.snippets.deleteComment);
 
   const handleAddComment = async (content: string) => {
     setIsSubmitting(true);
@@ -33,6 +36,20 @@ export default function CommentsCompponent({
     }
   };
 
+  const handleDeleteComment = async (commentId: Id<"snippetComments">) => {
+    setDeletingCommentId(commentId);
+
+    try {
+      await deleteComment({commentId});
+    } catch (error) {
+      console.log("Error deleting comment:" , error);
+      toast.error("Something went wrong");
+    } finally {
+      setDeletingCommentId(null);
+    }
+
+  };
+
   return (
     <div className="bg-[#121218] border border-[#ffffff0a] rounded-2xl overflow-hidden">
       <div className="px-6 sm:px-8 py-6 border-b border-[#ffffff0a]">
@@ -44,7 +61,10 @@ export default function CommentsCompponent({
 
       <div className="p-6 sm:p-8">
         {user ? (
-          <CommentFormComponent onSubmit={handleAddComment} isSubmitting={isSubmitting} />
+          <CommentFormComponent
+            onSubmit={handleAddComment}
+            isSubmitting={isSubmitting}
+          />
         ) : (
           <div className="bg-[#0a0a0f] rounded-xl p-6 text-center mb-8 border border-[#ffffff0a]">
             <p className="text-[#808086] mb-4">
@@ -58,7 +78,17 @@ export default function CommentsCompponent({
           </div>
         )}
 
-        <div>Display All comments here</div>
+        <div className="space-y-6">
+          {comments.map((comment) => (
+            <CommentComponent
+              key={comment._id}
+              comment={comment}
+              onDelete={handleDeleteComment}
+              isDeleting={deletingCommentId === comment._id}
+              currentUserId={user?.id}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
