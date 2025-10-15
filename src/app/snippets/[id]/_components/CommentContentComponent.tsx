983121e90de8d7f4cc11d8e694452d6ef18a451d
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-10-15 10:22:27 +0300

    feat: add CommentContentComponent for rendering formatted comments

--- src/app/snippets/[id]/_components/CommentContentComponent.tsx
+++ src/app/snippets/[id]/_components/CommentContentComponent.tsx
@@ -3,5 +3,28 @@ export default function CommentContentComponent({
 }: {
   content: string;
 }) {
-  return <div>hey</div>;
+  // integrate auto detect language later
+  const commentParts = content.split(/(```[\w-]*\n[\s\S]*?\n```)/g);
+
+  return (
+    <div className="max-w-none text-white">
+      {commentParts.map((part, i) => {
+        if (part.startsWith("```")) {
+          const match = part.match(/```([\w-]*)\n([\s\S]*?)\n```/);
+
+          if (match) {
+            const [language, code] = match;
+            // replace with code block comp
+            return <div key={i}>{language} {code}</div>;
+          }
+        }
+
+        return part.split("\n").map((line, lineIdx) => (
+          <p key={lineIdx} className="mb-4 text-gray-300 last:mb-0">
+            {line}
+          </p>
+        ));
+      })}
+    </div>
+  );
 }
